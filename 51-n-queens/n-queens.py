78e7edf1a3028bf755de6ec047f4cc6e2b232f6b
hirenpahad1 <hirenpahad@gmail.com>
2026-01-12 23:09:13 +0530

    Time: 4 ms (98.52%) | Memory: 19.8 MB (6.38%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,26 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        board =[['.' for _ in range(n)]for _ in range(n)]
+        col = [False] * (2*n -1)
+        diag1 = [False] *(2*n -1)
+        diag2 = [False] *(2*n -1)
+
+        def backtrack(row):
+            if row == n:
+                res.append(["".join(r) for r in board])
+                return
+            for c in range(n):
+                if col[c] or diag1[row - c+n-1] or diag2[row+c]:
+                    continue
+                board[row][c] ='Q'
+                col[c]= diag1[row-c+n-1] = diag2[row+c] = True
+                backtrack(row+1)
+                board[row][c] ='.'
+                col[c]= diag1[row-c+n-1] = diag2[row+c] = False
+
+        backtrack(0)
+        return res
+                
+
+
